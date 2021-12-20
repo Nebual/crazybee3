022edd66ffa79f744516db5c9dcb551831737834
Nebual <ben1120@gmail.com>
2021-12-20 03:49:25 -0800

    Seed random number generator each game

--- Main.gd
+++ Main.gd
@@ -5,6 +5,7 @@ export var start_paused = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize() # Randomizes the seed of the random number generator
 	if start_paused:
 		get_tree().paused = true
 
